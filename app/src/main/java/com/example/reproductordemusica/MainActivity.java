c75ef6e789c6983bc4b6f7727947b110bf03d0ab
Diego Caballero <isaaccollado12@gmail.com>
2024-06-06 18:39:18 -0500

    Actualizaciones de la aplicacion
    Se corrigieron los nombres de la pistas, falta añadir la imagen faltante que sera usado para el boton de no repetir, incluir el uso de la biblioteca, menu de Home e incluir la pantalla de casa donde muestre las canciones por reproducir

--- app/src/main/java/com/example/reproductordemusica/MainActivity.java
+++ app/src/main/java/com/example/reproductordemusica/MainActivity.java
@@ -1,12 +1,14 @@
 package com.example.reproductordemusica;
+
 import android.annotation.SuppressLint;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.graphics.drawable.GradientDrawable;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.SeekBar;
@@ -19,8 +21,6 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.palette.graphics.Palette;
-import android.graphics.drawable.GradientDrawable;
-import android.graphics.Color;
 
 public class MainActivity extends AppCompatActivity {
     // Componentes de la UI
@@ -36,22 +36,18 @@ public class MainActivity extends AppCompatActivity {
     TextView song, artist;
     TextView tiempoTranscurridoTextView;
     TextView tiempoRestanteTextView;
-
-
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-
         // Ajustar la UI para mostrar en pantalla completa
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
         // Inicializar los componentes de la UI
         mainLayout = findViewById(R.id.main); // Referencia al layout principal
         play_pause = findViewById(R.id.play_pause); // Botón de reproducir/pausar
@@ -63,26 +59,21 @@ public class MainActivity extends AppCompatActivity {
         artist = findViewById(R.id.artist); //Textview del nombre del artista
         tiempoTranscurridoTextView = findViewById(R.id.tiempoTranscurrido);
         tiempoRestanteTextView = findViewById(R.id.tiempoRestante);
-
-
         // Inicializar los reproductores de medios
         initializeMediaPlayers();
         // Configurar el listener del SeekBar
         setupSeekBar();
         // Configurar el listener de finalización del MediaPlayer
         setupMediaCompletionListener();
-
         // Configurar el fondo degradado inicial
         updateBackgroundColor(R.drawable.caratula); // Usa la carátula inicial como referencia
     }
-
     // Inicializar las instancias de MediaPlayer con los recursos de audio
     private void initializeMediaPlayers() {
         vectormp[0] = MediaPlayer.create(this, R.raw.manifiesto);
         vectormp[1] = MediaPlayer.create(this, R.raw.mas_de_lo_mismo);
         vectormp[2] = MediaPlayer.create(this, R.raw.tamo_es_pa_gozar);
     }
-
     // Configurar el listener de cambios del SeekBar
     private void setupSeekBar() {
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
@@ -92,7 +83,6 @@ public class MainActivity extends AppCompatActivity {
                     vectormp[posicion].seekTo(i);
                 }
             }
-
             @Override
             public void onStartTrackingTouch(SeekBar seekBar) {}
 
@@ -100,7 +90,6 @@ public class MainActivity extends AppCompatActivity {
             public void onStopTrackingTouch(SeekBar seekBar) {}
         });
     }
-
     // Configurar el listener de finalización del MediaPlayer para manejar las transiciones de pistas
     private void setupMediaCompletionListener() {
         for (int i = 0; i < vectormp.length; i++) {
@@ -118,7 +107,6 @@ public class MainActivity extends AppCompatActivity {
             });
         }
     }
-
     // Reproducir o pausar la pista actual
     public void PlayPause(View view) {
         if (vectormp[posicion].isPlaying()) {
@@ -133,7 +121,6 @@ public class MainActivity extends AppCompatActivity {
             updateSeekbar();
         }
     }
-
     // Alternar la funcionalidad de repetición para la pista actual
     public void Repetir(View view) {
         if (repetir == 1) {
@@ -148,7 +135,6 @@ public class MainActivity extends AppCompatActivity {
             repetir = 1;
         }
     }
-
     // Saltar a la siguiente pista
     public void Siguiente(View view) {
         if (posicion < vectormp.length - 1) {
@@ -163,7 +149,6 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(this, "No hay más canciones", Toast.LENGTH_SHORT).show();
         }
     }
-
     // Volver a la pista anterior
     public void Anterior(View view) {
         if (posicion >= 1) {
@@ -179,45 +164,40 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(this, "No hay más canciones", Toast.LENGTH_SHORT).show();
         }
     }
-
     // Reinicializar las instancias de MediaPlayer (útil al cambiar de pista)
     private void resetMediaPlayers() {
         vectormp[0] = MediaPlayer.create(this, R.raw.manifiesto);
         vectormp[1] = MediaPlayer.create(this, R.raw.mas_de_lo_mismo);
         vectormp[2] = MediaPlayer.create(this, R.raw.tamo_es_pa_gozar);
     }
-
-
     // Actualizar los componentes de la UI en función de la pista actual
     private void updateUI() {
         int imageResId;
         switch (posicion) {
             case 0:
                 imageResId = R.drawable.caratula;
-                song.setText("Test Track 1");
-                artist.setText("Diego Caballero");
+                song.setText("Manifiesto");
+                artist.setText("La Scene");
                 break;
             case 1:
                 imageResId = R.drawable.portada_1;
-                song.setText("Test Track 2");
-                artist.setText("Diego Caballero y alguien más");
+                song.setText("Mas de lo Mismo");
+                artist.setText("Entrenos");
                 break;
             case 2:
                 imageResId = R.drawable.portada_2;
-                song.setText("Test Track 3");
-                artist.setText("Diego Caballero y sus colegas");
+                song.setText("Tamos es pa gozar");
+                artist.setText("Mecanik Informal");
                 break;
             default:
                 imageResId = R.drawable.caratula;
                 song.setText("Test Track DEFAULT");
-                artist.setText("Diego Caballero DEFAULT");
+                artist.setText("Ponganos una A");
                 break;
         }
         iv.setImageResource(imageResId);
         updateBackgroundColor(imageResId);
     }
-
-
     // Actualizar el color de fondo basado en la carátula del álbum
     private void updateBackgroundColor(int imageResId) {
         Bitmap bitmap = BitmapFactory.decodeResource(getResources(), imageResId);
@@ -237,9 +217,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-
-
-
     // Oscurecer un color reduciendo su brillo
     private int darkenColor(int color) {
         float[] hsv = new float[3];
@@ -247,15 +224,11 @@ public class MainActivity extends AppCompatActivity {
         hsv[2] *= 20f; // Reducir el brillo para oscurecer el color
         return Color.HSVToColor(hsv);
     }
-
-
     // Verificar si un color es claro o oscuro
     private boolean isColorLight(int color) {
         double darkness = 1 - (0.299 * Color.red(color) + 0.587 * Color.green(color) + 0.114 * Color.blue(color)) / 255;
         return darkness < 0.5;
     }
-
-
     // Actualizar el SeekBar periódicamente para reflejar la posición de reproducción
     public void updateSeekbar() {
         seekBar.setMax(vectormp[posicion].getDuration());
@@ -284,7 +257,6 @@ public class MainActivity extends AppCompatActivity {
         tiempoTranscurridoTextView.setText(formatearTiempo(posicionActual));
         tiempoRestanteTextView.setText(String.format("%s", formatearTiempo(duracionTotal - posicionActual)));
     }
-
     // Liberar los recursos del MediaPlayer cuando la actividad es destruida
     @Override
     protected void onDestroy() {
