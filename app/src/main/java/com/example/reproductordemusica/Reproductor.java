5f7692637ce7c3bb0aaebad368ae9fd8461919ab
Diego Caballero <isaaccollado12@gmail.com>
2024-07-10 23:15:46 -0500

    Acutalizacion

--- app/src/main/java/com/example/reproductordemusica/Reproductor.java
+++ app/src/main/java/com/example/reproductordemusica/Reproductor.java
@@ -1,5 +1,4 @@
 package com.example.reproductordemusica;
-import com.example.reproductordemusica.MainActivity;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
@@ -8,15 +7,14 @@ import android.graphics.drawable.GradientDrawable;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Looper;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
-import android.os.Looper;
-import com.google.android.material.snackbar.Snackbar;
-import jp.wasabeef.glide.transformations.RoundedCornersTransformation;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -27,6 +25,7 @@ import androidx.palette.graphics.Palette;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.target.CustomTarget;
 import com.bumptech.glide.request.transition.Transition;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -36,6 +35,8 @@ import com.google.firebase.database.ValueEventListener;
 
 import java.io.IOException;
 import java.util.ArrayList;
+
+import jp.wasabeef.glide.transformations.RoundedCornersTransformation;
 
 public class Reproductor extends AppCompatActivity implements View.OnClickListener {
 
